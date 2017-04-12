9fa6f9331d6412c556b92ff6032c67c58b4ec892
Luis Antonio Cárdenas Lucio <i.s.c.luislucio90@gmail.com>
2017-04-12 14:51:40 -0500

    Up

---
+++ app/src/main/java/com/example/lepra/toolbar_tabs/activities/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.lepra.toolbar_tabs.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.example.lepra.toolbar_tabs.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
---
+++ app/src/main/java/com/example/lepra/toolbar_tabs/fragments/FirstFragment.java
@@ -0,0 +1,30 @@
+package com.example.lepra.toolbar_tabs.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lepra.toolbar_tabs.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        return view;
+    }
+
+}
